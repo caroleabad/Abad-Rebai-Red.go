1b45a4ca749f988f194e8f6840c88e4d293537f7
rihem <rihem.rebai@ynov.com>
2023-09-28 09:25:03 +0200

    test

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -16,16 +17,17 @@ type personage struct {
 	vieactuel  int
 	inventaire map[string]int
 	skill      []string
+	argent     int
 }
 
-func (p1 *personage) init(name string, class string, level int, viemax int, vieactuel int, inventaire map[string]int, skill []string) {
+func (p1 *personage) init(name string, class string, level int, viemax int, vieactuel int, sort string, argent int) {
 	p1.name = name
 	p1.class = class
 	p1.level = level
 	p1.viemax = viemax
 	p1.vieactuel = vieactuel
-	p1.inventaire = inventaire
-	p1.skill = skill
+	p1.skill = append(p1.skill, sort)
+	p1.argent = argent
 }
 
 func (p *personage) potion() {
@@ -41,12 +43,13 @@ func (p *personage) potion() {
 func main() {
 
 	var p personage
-	var p1 personage
-
-	p.init("caROle", "Elfe", 1, 100, 80, map[string]int{"potions": 1, "clefs": 5, "hache": 1, "potions de poison": 1}, []string{"coup de poing"})
-	p1.init("Rihem", "dragon", 1, 100, 40, map[string]int{"boule de feu": 3, " potion": 1, "épée": 1, "bouclier": 1}, []string{"voler dans les airs"})
-	p.charCreation()
-	p.Menu()
+	//var p1 personage
+	/*
+		p.init("caROle", "Elfe", 1, 100, 80, map[string]int{"potions": 1, "clefs": 5, "hache": 1, "potions de poison": 1}, []string{"coup de poing"})
+		p1.init("Rihem", "humain,elfe,nain", 1, 100, 40, map[string]int{"boule de feu": 3, " potion": 1, "épée": 1, "bouclier": 1}, []string{"voler dans les airs"})
+		p.charCreation()
+		p.Menu()*/
+	p.initPerso() 
 
 	/*  println("1 Jouer")
 	println("2 Marchand")
@@ -86,24 +89,28 @@ func main() {
 }
 
 func (p personage) Menu() {
-	println("1 Afficher les informations du joueur")
-	println("2 Afficher inventaire")
-	println("3 Quitter")
-	time.Sleep(1 * time.Second)
-	input := WaitForInput()
+	out := false
+	for !out {
+		fmt.Println("1 Afficher les informations du joueur")
+		fmt.Println("2 Afficher inventaire")
+		fmt.Println("3 Quitter")
+		time.Sleep(1 * time.Second)
+		input := WaitForInput()
 
-	switch input {
-	case "1":
-		p.DisplayInfo()
-		p.Menu()
-	case "2":
-		p.AccessInventory()
-		p.Menu()
-	case "3":
-		println("Quitter")
-	default:
-		println("Mauvaise réponse")
-		break
+		switch input {
+		case "1":
+			p.DisplayInfo()
+			p.Menu()
+		case "2":
+			p.AccessInventory()
+			p.Menu()
+		case "3":
+			fmt.Println("Quitter")
+			out = true
+
+		default:
+			fmt.Println("Mauvaise réponse")
+		}
 	}
 }
 
@@ -207,26 +214,53 @@ func (p *personage) CheckSpell(spellName string) bool {
 	return false
 }
 func (p *personage) Marchand() {
-	println("Que voulez-vous acheter ?")
-	println("1 Potion")
-	println("2 clef")
-	println("3 hache")
-	println("4 boule de feu")
-	reponse := WaitForInput()
-	if reponse == "1" {
-		p.inventaire["potions"]++
-		println("vous avez bien acheter")
-	} else if reponse == "2" {
-		p.inventaire["clefs"]++
-		println("vous avez bien acheter")
-	} else if reponse == "3" {
-		p.inventaire["hache"]++
-		println("vous avez bien acheter")
-	} else if reponse == "4" {
-		p.inventaire["boule de feu"]++
-		println("vous avez bien acheter")
+	for i, v := range listeInventaire {
+		fmt.Println((i + 1), " ", v.name, " ", v.price, "$")
 	}
+	var nb int
+	var isCorrect bool
+	for !isCorrect {
+		var err error
+		input := WaitForInput()
+		nb, err = strconv.Atoi(input)
+		if err == nil && nb > 0 && nb <= len(listeInventaire) {
+			isCorrect = true
+		} else {
+			fmt.Println("il faut rentrer un nombre")
+		}
+	}
+
+	if p.argent >= listeInventaire[nb-1].price {
+		fmt.Println("vous avez acheter quelque chose")
+		p.argent -= listeInventaire[nb-1].price
+		p.inventaire[listeInventaire[nb-1].name] += 1
+	} else {
+		fmt.Println("vous n'avez pas assez d'argent ")
+
+	}
+
 }
+
+//fmt.Println("Que vouliez-vous acheter ?")
+//fmt.Print("1 Potion")
+//fmt.Println("2 clef")
+//fmt.Println("3 hache")
+//fmt.Println("4 boule de feu")
+//reponse := WaitForInput()
+//if reponse == "1" {
+//	p.inventaire["potions"]++
+//	fmt.Println("vous avez bien acheter")
+//} else if reponse == "2" {
+//	p.inventaire["clefs"]++
+//	fmt.Println("vous avez bien acheter")
+//} else if reponse == "3" {
+//	p.inventaire["hache"]++
+//	fmt.Println("vous avez bien acheter")
+//} else if reponse == "4" {
+//	p.inventaire["boule de feu"]++
+//	fmt.Println("vous avez bien acheter")
+//}
+
 func (p1 *personage) charCreation() {
 
 	a := ""
@@ -247,4 +281,79 @@ func (p1 *personage) charCreation() {
 	}
 	p1.name = a
 
+}
+func (p *personage) initPerso() {
+	fmt.Println("entrez votre nom:")
+	var name string
+	fmt.Scan(&name)
+	var pviemax int
+	var pvieactuel int
+	var class string
+	var level int
+	var sort string
+	fmt.Println("choisissez votre classe:")
+	fmt.Scan(&class)
+
+	switch class {
+	case "humain":
+		class = "humain"
+		pviemax = 100
+		pvieactuel = 50
+		level = 1
+		sort = "coup de poing"
+
+	case "elfe":
+		class = "elfe"
+		pviemax = 80
+		pvieactuel = 40
+		level = 1
+		sort = "coup de poing"
+
+	case "nain":
+		class = "nain"
+		pviemax = 120
+		pvieactuel = 60
+		level = 1
+		sort = "coup de poing"
+
+	default:
+		println("classe invalide, choisissez parmi humain , elfe ou nain ")
+	}
+	p.init(name, class, level, pviemax, pvieactuel, sort, 100)
+	p.charCreation()
+	p.Menu()
+
+}
+
+func (p *personage) ajouterObjet(objet string) {
+
+	if len(p.inventaire) <= 10 || p.inventaire[objet] > 0 {
+		p.inventaire[objet] += 1
+		fmt.Println("a été ajouté à l'inventaire.\n", objet)
+	} else {
+		fmt.Println("L'inventaire est plein, vous ne pouvez pas ajouter plus d'objets.")
+	}
+}
+
+type product struct {
+	name  string
+	price int
+}
+
+var potionDeVie = product{
+	name:  "Potion de vie",
+	price: 3,
+}
+var potionDePoison = product{
+	name:  "potion de poison ",
+	price: 6,
+}
+var livreDeSort = product{
+	name:  "livre de sort: boule de feu ",
+	price: 25,
+}
+var listeInventaire = []product{
+	potionDeVie,
+	potionDePoison,
+	livreDeSort,
 }
